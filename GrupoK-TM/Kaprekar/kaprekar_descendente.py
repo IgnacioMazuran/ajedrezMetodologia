19942ebd71b65994e0b73d9b857eb4fd8d3fbfe2
Maurovalls <62670952+Maurovalls@users.noreply.github.com>
2020-06-04 16:43:00 -0300

    Update kaprekar_descendente.py

--- GrupoK-TM/Kaprekar/kaprekar_descendente.py
+++ GrupoK-TM/Kaprekar/kaprekar_descendente.py
@@ -1,3 +1,16 @@
 # Ordena en forma descendente
-def descencente(num):
-    return int(''.join(sorted(str(num))[::-1]))
+# Completa con 0 si tiene menos de 4 cifras
+def descencente(n):
+	num = str(n)
+
+	if len(num)==4:
+		sNum = num
+	elif len(num)==3:
+		sNum = num+"0"
+	elif len(num)==2:
+		sNum = num+"00"
+	elif len(num)==1:
+		sNum = num+"000"
+
+	sNum = ''.join(sorted(str(sNum))[::-1])
+	return sNum
