881a769f41d537072ce1687cac8dd0e9af2cc46f
GabrielAlaniz <62668410+GabrielAlaniz@users.noreply.github.com>
2020-06-04 15:29:03 -0300

    Update kaprekar_master.py
    Comprobaciones agregadas

--- GrupoK-TM/Kaprekar/kaprekar_master.py
+++ GrupoK-TM/Kaprekar/kaprekar_master.py
@@ -1,19 +1,22 @@
 from kaprekar_mostrarResultado import mostrarResultado
 from kaprekar_ascendente import ascendente
 from kaprekar_descendente import descencente
-# Bucle con contador
-contador = 0
 
 n = input("Ingresa un numero de 4 digitos: ")
+# Compruebo que tenga al menos 2 cifras diferentes
+# Comprueba que no tenga mas de 4 cifras en total
+# Comprueba que no sea un numero negativo
+while(int(n)<0 or int(n)>9998 or int(n)%1111==0):
+	n = input("Ingreso incorrecto!! Ingrese nuevamente: ")
+
+# bucle con contador que ordena los métodos y realiza la operación:
 print ("\nOrdenando ", n)
-
-# Bucle con contador
 contador = 0
-# Llamada a los métodos en orden
-while n != descencente(n) - ascendente(n):
-    print (descencente(n), "-", ascendente(n), "=", descencente(n)-ascendente(n))
-    n = descencente(n) - ascendente(n)
-    contador+=1
-
+while n != 6174:
+	nDes = int(descencente(n))
+	nAsc = int(ascendente(n))
+	n = nDes - nAsc
+	print(nDes," - ",nAsc," = ", n)
+	contador+=1
 
 mostrarResultado(n,contador)
