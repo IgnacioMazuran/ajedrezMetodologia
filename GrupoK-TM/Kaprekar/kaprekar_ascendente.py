210107c69d3b75cac399cbac874a4bad67d64d1d
IgnacioMazuran <49373818+IgnacioMazuran@users.noreply.github.com>
2020-06-05 09:51:19 -0300

    Update kaprekar_ascendente.py

--- GrupoK-TM/Kaprekar/kaprekar_ascendente.py
+++ GrupoK-TM/Kaprekar/kaprekar_ascendente.py
@@ -1,4 +1,18 @@
 # Ordena el input del usuario en forma ascendente
-def ascendente(num):
-    return int(''.join(sorted(str(num))))
+# Completa con 0 si tiene menos de 4 cifras
+def ascendente(n):
+	num = str(n)
+	
+	if len(num)==4:
+		sNum = num
+	elif len(num)==3:
+		sNum = "0"+num
+	elif len(num)==2:
+		sNum = "00"+num
+	elif len(num)==1:
+		sNum = "000"+num
+
+	sNum = ''.join(sorted(str(sNum)))
+	return sNum
+    
     
