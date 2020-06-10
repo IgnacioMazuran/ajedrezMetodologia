5e202c28879a3563c1433c247c73ec33f0bb06e0
GabrielAlaniz <62668410+GabrielAlaniz@users.noreply.github.com>
2020-06-10 12:13:26 -0300

    Update kaprekar_master.py

--- GrupoK-TM/Kaprekar/kaprekar_master.py
+++ GrupoK-TM/Kaprekar/kaprekar_master.py
@@ -6,23 +6,32 @@ casosDePrueba = int(input("Cuantos numeros desea testear? "))
 caso=0
 
 while(caso<casosDePrueba):
-	n = input("Ingresa un numero de 4 digitos: ")
+	n = input("\nIngresa un numero de 4 digitos: ")
 	# Compruebo que tenga al menos 2 cifras diferentes
 	# Comprueba que no tenga mas de 4 cifras en total
 	# Comprueba que no sea un numero negativo
-	while(int(n)<0 or int(n)>9998 or int(n)%1111==0):
-		n = input("El numero no puede tener dos digitos iguales!! Ingrese nuevamente: ")
+	while(int(n)<0 or int(n)>9998):
+		n = input("Ingreso incorrecto!! Ingrese nuevamente: ")
 
-	# bucle con contador que ordena los métodos y realiza la operación:
-	print ("\nOrdenando ", n)
-	contador = 0
-	while n != 6174:
-		nDes = int(descencente(n))
-		nAsc = int(ascendente(n))
-		n = nDes - nAsc
-		print(nDes," - ",nAsc," = ", n)
-		contador+=1
 	
-	mostrarResultado(n,contador)
-	caso+=1
-	print("Numero: ",caso," de ",casosDePrueba)
+	contador = 0
+	if int(n)%1111==0:
+		print("DEBE CONTENER, AL MENOS DOS CIFRAS DIFERENTES!!")
+		caso+=1
+	elif n=="6174":
+		print("Numero ingresado es la constante")
+		caso+=1
+	else:
+		# bucle con contador que ordena los métodos y realiza la operación:
+		print ("\nOrdenando ", n)
+		while n != 6174:
+			nDes = int(descencente(n))
+			nAsc = int(ascendente(n))
+			n = nDes - nAsc
+			print(nDes," - ",nAsc," = ", n)
+			contador+=1
+		mostrarResultado(n,contador)
+		caso+=1
+	print("--------------------------------------------")
+	print("Intento numero: ",caso," de ",casosDePrueba)
+	print("--------------------------------------------")
