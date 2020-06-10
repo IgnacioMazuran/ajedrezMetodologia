be135e47233f7860528bd58cd20a2c1cfd161c20
GabrielAlaniz <62668410+GabrielAlaniz@users.noreply.github.com>
2020-06-10 11:21:58 -0300

    Update kaprekar_master.py

--- GrupoK-TM/Kaprekar/kaprekar_master.py
+++ GrupoK-TM/Kaprekar/kaprekar_master.py
@@ -2,21 +2,27 @@ from kaprekar_mostrarResultado import mostrarResultado
 from kaprekar_ascendente import ascendente
 from kaprekar_descendente import descencente
 
-n = input("Ingresa un numero de 4 digitos: ")
-# Compruebo que tenga al menos 2 cifras diferentes
-# Comprueba que no tenga mas de 4 cifras en total
-# Comprueba que no sea un numero negativo
-while(int(n)<0 or int(n)>9998 or int(n)%1111==0):
-	n = input("Ingreso incorrecto!! Ingrese nuevamente: ")
+casosDePrueba = int(input("Cuantos numeros desea testear? "))
+caso=0
 
-# bucle con contador que ordena los métodos y realiza la operación:
-print ("\nOrdenando ", n)
-contador = 0
-while n != 6174:
-	nDes = int(descencente(n))
-	nAsc = int(ascendente(n))
-	n = nDes - nAsc
-	print(nDes," - ",nAsc," = ", n)
-	contador+=1
+while(caso<casosDePrueba):
+	n = input("Ingresa un numero de 4 digitos: ")
+	# Compruebo que tenga al menos 2 cifras diferentes
+	# Comprueba que no tenga mas de 4 cifras en total
+	# Comprueba que no sea un numero negativo
+	while(int(n)<0 or int(n)>9998 or int(n)%1111==0):
+		n = input("Ingreso incorrecto!! Ingrese nuevamente: ")
 
-mostrarResultado(n,contador)
+	# bucle con contador que ordena los métodos y realiza la operación:
+	print ("\nOrdenando ", n)
+	contador = 0
+	while n != 6174:
+		nDes = int(descencente(n))
+		nAsc = int(ascendente(n))
+		n = nDes - nAsc
+		print(nDes," - ",nAsc," = ", n)
+		contador+=1
+	
+	mostrarResultado(n,contador)
+	caso+=1
+	print("Numero: ",caso," de ",casosDePrueba)
