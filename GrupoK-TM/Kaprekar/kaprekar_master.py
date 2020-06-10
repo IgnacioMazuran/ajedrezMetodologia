9b8267a3dc9828ca314cedd8cd882d6a24cc31b9
IgnacioMazuran <49373818+IgnacioMazuran@users.noreply.github.com>
2020-06-10 12:03:23 -0300

    Update kaprekar_master.py

--- GrupoK-TM/Kaprekar/kaprekar_master.py
+++ GrupoK-TM/Kaprekar/kaprekar_master.py
@@ -11,7 +11,7 @@ while(caso<casosDePrueba):
 	# Comprueba que no tenga mas de 4 cifras en total
 	# Comprueba que no sea un numero negativo
 	while(int(n)<0 or int(n)>9998 or int(n)%1111==0):
-		n = input("Ingreso incorrecto!! Ingrese nuevamente: ")
+		n = input("El numero no puede tener dos digitos iguales!! Ingrese nuevamente: ")
 
 	# bucle con contador que ordena los métodos y realiza la operación:
 	print ("\nOrdenando ", n)
