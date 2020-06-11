e13411e20b164303ccb1028054f003832ffbef2a
GabrielAlaniz <gabriel.alaniz89@gmail.com>
2020-06-11 02:08:00 -0300

    Prueba

--- GrupoK-TM/Proyecto/Juega.py
+++ GrupoK-TM/Proyecto/Juega.py
@@ -18,3 +18,49 @@ class Juega:
 
     def get_mov_list(self):
         return ' '.join(self.movimientos_historial)
+
+    def to_FEN(self):
+        FEN_str = ''
+
+        for i in range(len(RANGOS)):
+            primer = len(self.tablero) - 8*(i+1)
+            vacio_casill = 0
+            for fille in range(len(FILAS)):
+                pieza = self.tablero[primer+fille]
+                if pieza&PIEZA_MASK == VACIO:
+                    vacio_casill += 1
+                else:
+                    if vacio_casill > 0:
+                        FEN_str += '{}'.format(vacio_casill)
+                    FEN_str += '{}'.format(pieza_str(pieza))
+                    vacio_casill = 0
+            if vacio_casill > 0:
+                FEN_str += '{}'.format(vacio_casill)
+            FEN_str += '/'
+        FEN_str = FEN_str[:-1] + ' '
+
+        if self.mueve_prim == BLANCO:
+            FEN_str += 'b '
+        if self.mueve_prim == NEGRO:
+            FEN_str += 'n '
+
+        if self.enroque_reglas & ENROCAR_KINGSIDE_BLANCO:
+            FEN_str += 'K'
+        if self.enroque_reglas & ENROCAR_QUEENSIDE_BLANCO:
+            FEN_str += 'Q'
+        if self.enroque_reglas & ENROCAR_KINGSIDE_NEGRO:
+            FEN_str += 'k'
+        if self.enroque_reglas & ENROCAR_QUEENSIDE_NEGRO:
+            FEN_str += 'q'
+        if self.enroque_reglas == 0:
+            FEN_str += '-'
+        FEN_str += ' '
+
+        if self.ep_casill == 0:
+            FEN_str += '-'
+        else:
+            FEN_str += bb2str(self.ep_casill)
+
+        FEN_str += ' {}'.format(self.halfmove_clock)
+        FEN_str += ' {}'.format(self.fullmove_number)
+        return FEN_str
