fa89007467a6bdcade9f6a41b651b6444d30556a
IgnacioMazuran <ignaciomazuran12@gmail.com>
2020-06-11 01:35:36 -0300

    Test

---
+++ GrupoK-TM/Proyecto/Juega.py
@@ -0,0 +1,20 @@
+class Juega:
+    def __init__(self, FEN=''):
+        self.tablero = TABLERO_INICIO
+        self.mueve_prim = BLANCO
+        self.ep_casill = 0
+        self.enroque_reglas = FULL_ENROQUE_REGLAS
+        self.halfmove_clock = 0
+        self.fullmove_number = 1
+
+        self.posicion_historial = []
+        if FEN != '':
+            self.load_FEN(FEN)
+            self.posicion_historial.append(FEN)
+        else:
+            self.posicion_historial.append(INICIAL_FEN)
+            
+        self.movimientos_historial = []
+
+    def get_mov_list(self):
+        return ' '.join(self.movimientos_historial)
